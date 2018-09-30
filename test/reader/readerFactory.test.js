9ecf832ec11688532acf61c3ad89033fbf775795
Bipo Chen <bipoegg@gmail.com>
2018-09-30 17:56:19 +0800

    [What] add test case for all kinds of reader Part II

---
+++ test/reader/readerFactory.test.js
@@ -0,0 +1,70 @@
+'use restrict'
+
+var assert = require('chai').assert;
+var ReaderType = require('../../lib/reader/readerType');
+var ReaderFactory = require('../../lib/reader/readerFactory');
+var FileReader = require('../../lib/reader/fileReader');
+var StreamReader = require('../../lib/reader/streamReader');
+
+describe('ReaderFactory class', function () {
+
+  it('error if option not be assigned', function () {
+    assert.throws(() => ReaderFactory.getReader(), 'Need pass reader option');
+  });
+
+  it('error when the reader type not assign', function () {
+    var readerOption = {
+      path: null
+    }
+    assert.throws(() => ReaderFactory.getReader(readerOption), 'not supported reader type');
+  });
+
+  it('error when the reader type is assign right', function () {
+    var readerOption = {
+      type: null,
+      path: null
+    }
+    assert.throws(() => ReaderFactory.getReader(readerOption), 'not supported reader type');
+  });
+
+  it('error when the reader type is not support', function () {
+    var readerOption = {
+      type: 'not_support',
+      path: null
+    }
+    assert.throws(() => ReaderFactory.getReader(readerOption), 'not supported reader type');
+  });
+
+  it('error when the reader path not assign', function () {
+    var readerOption = {
+      type: ReaderType.FILE
+    }
+    assert.throws(() => ReaderFactory.getReader(readerOption), 'path not exist');
+  });
+
+  it('error when the reader path not assign right', function () {
+    var readerOption = {
+      type: ReaderType.FILE,
+      path: null
+    }
+    assert.throws(() => ReaderFactory.getReader(readerOption), 'path not exist');
+  });
+
+  it('fileReader will be generated successfully when option is right', function () {
+    var readerOption = {
+      type: ReaderType.FILE,
+      path: __dirname + '/test.html'
+    }
+    var fileReader = ReaderFactory.getReader(readerOption);
+    assert.instanceOf(fileReader, FileReader, 'fileReader is instance of FileReader');
+  });
+
+  it('streamReader will be generated successfully when option is right', function () {
+    var readerOption = {
+      type: ReaderType.STREAM,
+      path: __dirname + '/test.html'
+    }
+    var streamReader = ReaderFactory.getReader(readerOption);
+    assert.instanceOf(streamReader, StreamReader, 'streamReader is instance of StreamReader');
+  });
+});
---
+++ test/reader/streamReader.test.js
@@ -0,0 +1,30 @@
+'use restrict'
+
+var assert = require('chai').assert;
+var StreamReader = require('../../lib/reader/streamReader');
+
+describe('StreamReader class', function () {
+
+  it('error if path not be assigned', function () {
+    assert.throws(() => new StreamReader(), 'path not exist');
+  });
+
+  it('set path success', function () {
+    const path = './test.html';
+    const streamReader = new StreamReader(path);
+    assert.equal(streamReader.path, path);
+  });
+
+  it('error for not existing file', function () {
+    const streamReader = new StreamReader(__dirname + '/notExist.html');
+    streamReader
+      .read()
+      .then(
+        result => {
+          throw new Error('File not exist, Error');
+        },
+        err => {
+          assert.isDefined(err);
+        });
+  });
+});
