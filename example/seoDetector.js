79ff396384e38fc3aa941af086962d8c92ccf3eb
Bipo Chen <bipoegg@gmail.com>
2018-09-30 19:08:28 +0800

    [What] add ruleFn for ruleOption to make user can customized rule not define in supportedRuleEngine. Part End

--- example/seoDetector.js
+++ example/seoDetector.js
@@ -47,6 +47,24 @@ for (var i in DefaultRule) {
 // customized parameter with supported rule engine
 ruleOptions.push({name: 'customized1', engine: SupportedRuleEngine.tagWithoutAttrCount,
                   param: {root: '', tag:'foo', attr:'123'}});
+ruleOptions.push({name: 'customized2', engine: SupportedRuleEngine.tagExistence,
+                  param: {root: '', tag:'foo'}});
+ruleOptions.push({name: 'customized3', engine: SupportedRuleEngine.tagExistenceWithAttrValue,
+                  param: {root: '', tag:'foo', attr:'name', value:'seo'}});
+ruleOptions.push({name: 'customized4', engine: SupportedRuleEngine.tagLimitCount,
+                  param: {root: '', tag:'foo', limit:1}});
+ruleOptions.push({name: 'customized5', engine: 'bbb',
+                  param: {root: '', tag:'foo'}});
+
+// customized rule with customized rule engine
+ruleOptions.push({name: 'customizedRuleEngine', ruleFn : $ => {
+    var errors = '';
+    if (!$('aaa').length) {
+      errors = 'This html does not contains <aaa> tag!';
+    }
+   return errors;
+}});
+
 var seoDetector = new detector(streamOption.input, consoleOption.output, ruleOptions);
 
 seoDetector
