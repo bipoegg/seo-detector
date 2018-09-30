082a6134c444b1a493675c19b1be98c1992485eb
Bipo Chen <bipoegg@gmail.com>
2018-09-30 22:13:57 +0800

    [what] Add test case for the new rule tagExistenceWithAttrValue Part End of new rule tagExistenceWithAttrValue

---
+++ test/rule/tagExistenceWithAttrValue.test.js
@@ -0,0 +1,39 @@
+'use strict'
+
+var assert = require('chai').assert;
+var cheerio = require('cheerio');
+var RuleDetectLogic = require('../../lib/rule/ruleDetectLogic');
+
+
+describe('tagExistenceWithAttrValue method', function () {
+
+  var rootTag = 'head';
+  var tag = 'meta';
+  var attr = 'name';
+  var value = 'description';
+
+  it('input head has no defined attr value', function () {
+    const dom = cheerio.load(
+      `<html>
+          <head></head>
+      </html>`
+    );
+
+    var ruleFn = RuleDetectLogic.detectTagWithAttributeValue(rootTag, tag, attr, value);
+    assert.equal(ruleFn(dom), 'This HTML without <' + tag  + ' ' + attr + '=\'' + value + '\'> tag');
+
+  });
+
+  it('input head has title', function () {
+    const dom = cheerio.load(
+      `<html>
+          <head>
+            <meta name="description" content="" />
+          </head>
+      </html>`
+    );
+
+    var ruleFn = RuleDetectLogic.detectTagWithAttributeValue(rootTag, tag, attr, value);
+    assert.equal(ruleFn(dom), '');
+  });
+});
